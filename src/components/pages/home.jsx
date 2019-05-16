2464e02e4ab95cba1a56554397825a69733e1715
Jean-François Dessureault <jfdess@gmail.com>
2019-05-16 10:07:23 -0400

    Added content to homepage

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -3,9 +3,33 @@ import Typography from '@material-ui/core/Typography'
 
 function Home () {
   return (
-    <Typography component='h1' variant='h3'>
-      Home sweet home
-    </Typography>
+    <>
+      <Typography component='h1' variant='h3'>
+        SMS Dashboard Demo
+      </Typography>
+      <p>This app allows you to:</p>
+      <ul>
+        <li>Mass send SMS to distribution lists</li>
+        <li>Target an audience for each message</li>
+        <li>Browse your distribution lists</li>
+        <li>Browse your messages history</li>
+      </ul>
+      <p>
+        This demo app uses static fake data to illustrate its functionnalities.
+        You cannot send SMS with it unless you hook it with your own backend.
+        Please visit the{' '}
+        <a
+          href='https://github.com/jeffdess/sms-dashboard'
+          title='SMS Dashboard on Github'
+          target='blank'
+        >
+          Github repository
+        </a>{' '}
+        and read the documentation to learn how you can achieve this with
+        minimal configuration.
+      </p>
+      <p>To try the app, start by clicking the login button.</p>
+    </>
   )
 }
 
